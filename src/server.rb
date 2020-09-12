67126b5b7f288e7b277e40521be73e35f049ca3b
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2020-09-11 21:23:41 -0500

    minor refactor to server

--- src/server.rb
+++ src/server.rb
@@ -3,15 +3,21 @@ require 'socket'
 port = 8080
 server = TCPServer.new(port)
 
+request_number = 1
+
 puts "server listening on port: #{port}"
 while session = server.accept
   request = session.gets
+
+  puts "incoming request: #{request_number}"
   puts request
 
-  session.print "HTTP/1.1 200\r\n"
-  session.print "Content-Type: text/html\r\n"
-  session.print "\r\n"
-  session.print "Hello world! The time is #{Time.now}"
+  session.puts 'HTTP/1.1 200'
+  session.puts 'Content-Type: text/html'
+  session.puts
+  session.puts "Hello world! The time is #{Time.now}"
 
   session.close
+
+  request_number += 1
 end
